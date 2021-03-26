eb96a5cf95eac4507233ca38dce0cbb980063ffb
Vladislav228-prog <73558450+Vladislav228-prog@users.noreply.github.com>
2021-03-26 19:09:56 +0300

    Add project files.

---
+++ Bulk-Thumbnail-Creator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bulk_Thumbnail_Creator
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string filePath = @"E:\newphot"; 
+
+            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            FileInfo[] files = directoryInfo.GetFiles();
+
+            Task.Factory.StartNew(() => PhotoManager.RenameImages(files)).Wait();
+            Task.Factory.StartNew(() => PhotoManager.ResizeImages(filePath)).Wait();
+    
+            Console.WriteLine("Operations were completed!");
+            Console.WriteLine("Check path: " + filePath);
+
+            Console.ReadKey();
+        }
+    }
+}
