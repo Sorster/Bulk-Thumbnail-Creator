e62cccc4c2489b686efddec1c4a19cc56482d156
Vladislav228-prog <73558450+Vladislav228-prog@users.noreply.github.com>
2021-03-26 19:25:55 +0300

    Final version

--- Bulk-Thumbnail-Creator/PhotoManager.cs
+++ Bulk-Thumbnail-Creator/PhotoManager.cs
@@ -1,78 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bulk_Thumbnail_Creator
 {
     public class PhotoManager
     {
-        public static void RenameImages(FileInfo[] files)
+        public static void RenameImages(FileInfo[] files,  string newFilesName)
         {
-            Console.WriteLine("---Rename---");
-
-            string newFilesName;
-            Console.WriteLine("Input name for all files");
-            newFilesName = Console.ReadLine();
-
             int fileNumber = 1;
-            try
+
+            foreach (var item in files)
             {
-                foreach (var item in files)
-                {
-                    item.Rename($"{newFilesName}{fileNumber}.jpg");
-                    fileNumber++;
-                }
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("There was an exception!");
+                item.MoveTo(Path.Combine(item.Directory.FullName, $"{newFilesName}{fileNumber}.jpg"));
+                fileNumber++;
             }
         }
 
-        public static void ResizeImages(string filePath)
+        public static void ResizeImages(string filePath, int width, int heigth)
         {
-            Console.WriteLine("---Resize---");
-
-            Console.WriteLine("Please input new image width");
-            int width = GetNumber();
-
-            Console.WriteLine("Please input new image heigth");
-            int heigth = GetNumber();
-
             int fileIndex = 1;
-
             string[] files = Directory.GetFiles(filePath);
-            try
-            { 
-                foreach (var item in files)
-                {
-                    Bitmap bmp = new Bitmap(item);
-                    Image image = new Bitmap(bmp, width, heigth);
-                    image.Save($"{filePath}/{fileIndex}.jpg");
-                    fileIndex++;
-                }
-            }
-            catch
-            {
-                Console.WriteLine("Something went wrong!");
-            }
-        }
 
-        public static int GetNumber()
-        {
-            do
+            foreach (var item in files)
             {
-                Console.Write("Value: ");
-                int value;
-                if(int.TryParse(Console.ReadLine(), out value))
-                {
-                    return value;
-                }
-            } while (true);
+                Bitmap bmp = new Bitmap(item);
+                Image image = new Bitmap(bmp, width, heigth);
+                image.Save($"{filePath}/{fileIndex}.jpg");
+                fileIndex++;
+            }
         }
     }
 }
--- Bulk-Thumbnail-Creator/Program.cs
+++ Bulk-Thumbnail-Creator/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace Bulk_Thumbnail_Creator
@@ -12,18 +8,55 @@ namespace Bulk_Thumbnail_Creator
     {
         static void Main()
         {
-            string filePath = @"E:\newphot"; 
+            string filesPath = @"E:\newphot"; 
 
-            DirectoryInfo directoryInfo = new DirectoryInfo(filePath);
+            DirectoryInfo directoryInfo = new DirectoryInfo(filesPath);
             FileInfo[] files = directoryInfo.GetFiles();
 
-            Task.Factory.StartNew(() => PhotoManager.RenameImages(files)).Wait();
-            Task.Factory.StartNew(() => PhotoManager.ResizeImages(filePath)).Wait();
+            Task.Factory.StartNew(() => RenamePictures(files)).Wait();
+            Task.Factory.StartNew(() => ResizePictures(filesPath)).Wait();
     
             Console.WriteLine("Operations were completed!");
-            Console.WriteLine("Check path: " + filePath);
+            Console.WriteLine("Check path: " + filesPath);
 
             Console.ReadKey();
+        }
+
+        static void RenamePictures(FileInfo[] files)
+        {
+            Console.WriteLine("---Rename---");
+
+            string newFilesName;
+            Console.WriteLine("Input name for all files");
+            newFilesName = Console.ReadLine();
+
+            PhotoManager.RenameImages(files, newFilesName);
+        }
+
+        static void ResizePictures(string filePath)
+        {
+            Console.WriteLine("---Resize---");
+
+            Console.WriteLine("Please input new image width");
+            int width = GetNumber();
+
+            Console.WriteLine("Please input new image heigth");
+            int heigth = GetNumber();
+
+            PhotoManager.ResizeImages(filePath, width, heigth);
+        }
+
+        static int GetNumber()
+        {
+            do
+            {
+                Console.Write("Value: ");
+                int value;
+                if (int.TryParse(Console.ReadLine(), out value))
+                {
+                    return value;
+                }
+            } while (true);
         }
     }
 }
